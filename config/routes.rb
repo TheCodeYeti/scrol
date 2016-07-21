39137f8a53b218c6ae5360950acd796e3707afef
spencer <spencer.dossett@gmail.com>
2016-07-21 10:49:38 -0400

    added routes for user_sessions and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'user_sessions/create'
 
   get 'user_sessions/destroy'
-
+  root to: 'users#index'
+  resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
