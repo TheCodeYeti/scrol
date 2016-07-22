eada93d669ed8d6068c1c2fe3fc39db5001ffce6
thandon263 <ncubethando20@gmail.com>
2016-07-22 15:03:49 -0400

    Adding twitter secrets to yml file on application

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,12 +1,14 @@
 class TwitterController < ApplicationController
   def index
         client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "gJS2PbF7GizO5zKyfXvXmcZn7"
-        config.consumer_secret     = "q7lNXWudAe5Fnc8EfrHscfQjxL6xwjfOJgkrAMYUTb5aMHx0dA"
-        config.access_token        = "4892852285-IT6yr3XMg9ubV3ADKhlGMF0VC1PS5O8r7dNZaEE"
-        config.access_token_secret = "DAso1ydU9jwFay7P2qeHkSwnK1KL09sFv0p3jyXl6boAq"
-      end
+          config.consumer_key = Rails.application.secrets.twitter_consumer_key
+          config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
+          config.access_token = Rails.application.secrets.twitter_access_token
+          config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+        end
 
-      @tweets = client.search('tweet', count: 4, screen_name: 'http://www.twitter.com/@thando20')
+      @tweets = client.search('tweets', count: 10, limit: 4, screen_name: 'http://www.twitter.com/thandon20')
+
   end
+
 end
