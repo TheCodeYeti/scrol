3dfe418697f2acd13658579ea8fd9c49ccfd15d2
spencer <spencer.dossett@gmail.com>
2016-07-22 11:31:08 -0400

    associated Authentication class with User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery!
+  authenticates_with_sorcery! do |config|
+    config.authentication_class = Authentication
+  end
+
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attribues_for :authentications
 end
