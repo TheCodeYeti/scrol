30f38c26e3e58e2adfacb4de63b0db5f8851e11b
spencer <spencer.dossett@gmail.com>
2016-07-27 18:20:57 -0400

    bug fixes! everything was broken

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -14,13 +14,13 @@ module AuthHelper
 
   # Generates the login URL for the app.
   def get_login_url
-    client = OAuth2::Client.new(Figaro.env.OUTLOOK_APPLICATION_ID,
-                                Figaro.env.OUTLOOK_PASSWORD,
-                                :site => 'https://login.microsoftonline.com',
-                                :authorize_url => '/common/oauth2/v2.0/authorize',
-                                :token_url => '/common/oauth2/v2.0/token')
-
-    login_url = client.auth_code.authorize_url(:redirect_uri => authorize_url, :scope => SCOPES.join(' '))
+    # client = OAuth2::Client.new(Figaro.env.OUTLOOK_APPLICATION_ID,
+    #                             Figaro.env.OUTLOOK_PASSWORD,
+    #                             :site => 'https://login.microsoftonline.com',
+    #                             :authorize_url => '/common/oauth2/v2.0/authorize',
+    #                             :token_url => '/common/oauth2/v2.0/token')
+    #
+    # login_url = client.auth_code.authorize_url(:redirect_uri => authorize_url, :scope => SCOPES.join(' '))
 
   end
 
