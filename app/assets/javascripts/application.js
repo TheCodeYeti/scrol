d8616718766669b7d5dd381a5f0dd75c60b86d25
thandon263 <ncubethando20@gmail.com>
2016-07-28 16:48:11 -0400

    Addaing javascript to application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,35 @@ $(function(){
   $('.sidenav').on('mouseout', function(){
     $('.sidenav-bar').removeClass('open');
   });
+
+  $('.google-plus').on('click', function(){
+     alert("This is working!!")
+  });
+
+  //These are the navigation links
+  /******
+    render content on click on the home page
+    Using ajax calls to get data from the database
+  */
+
+  $('.user-icon').on('click', function(){
+    alert("This is also working !!!")
+  });
+
+  $('.youtube').on('click', function(){
+    alert("This can also be working hey!!!")
+  });
+
+  $('.twitter').on('click', function(){
+    alert("This happens to be working!!")
+  });
+
+  $('.facebook').on('click', function(){
+    alert("This happens to be working!!")
+  });
+
+  $('.instagram').on('click', function(){
+    alert("This happens to be working!!")
+  });
+
 });
