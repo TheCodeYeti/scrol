de1a053e1853fa6f90fcbb2860a38d6a771d2f22
shari-singh <shari.singh93@gmail.com>
2016-08-08 17:46:33 -0400

    added common style and labels to message-items

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,7 +2,7 @@ $(function(){
   $('fa-google-plus').on('click', function(e){
     e.preventDefault();
     if ($('gmail-item').hasClass('on')){
-      $('gmail-item').removeClass('on');
+      $('gmail').removeClass('on');
       $('gmail-item').fadeOut(200).addClass('off');
     } else {
       $('gmail-item').removeClass('off');
