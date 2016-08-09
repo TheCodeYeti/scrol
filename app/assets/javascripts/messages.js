cbf67bbbdba006f61eeef3704c64196652ff4301
shari-singh <shari.singh93@gmail.com>
2016-08-09 07:34:02 -0400

    jQuery filter messages works now. Added styling to messages

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -4,20 +4,34 @@ $(function(){
     e.preventDefault();
     var $this= $(this);
     if ($this.hasClass('fa-google-plus')){
-      $('div').filter(document.getElementsByClassName('gmail')).parent().toggleClass('on off');
+      $('div').filter(document.getElementsByClassName('gmail')).parent().toggleClass('on off', 1000, 'swing');
     }
 
     if ($this.hasClass('fa-github')){
-      $('div').filter(document.getElementsByClassName('github')).parent().toggleClass('on off');
+      $('div').filter(document.getElementsByClassName('github')).parent().toggleClass('on off', 1000, 'swing');
     }
 
     if ($this.hasClass('fa-twitter')){
-      $('div').filter(document.getElementsByClassName('twitter')).parent().toggleClass('on off');
+      $('div').filter(document.getElementsByClassName('twitter')).parent().toggleClass('on off', 1000, 'swing');
     }
 
     if ($this.hasClass('fa-envelope')){
-      $('div').filter(document.getElementsByClassName('outlook')).parent().toggleClass('on off');
+      $('div').filter(document.getElementsByClassName('outlook')).parent().toggleClass('on off', 1000, 'swing');
     }
 
   });
+  $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+
+            }
+
 });
